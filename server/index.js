0965490ba4ab346f7c3847810827f6b9f0dfa201
Mason Maag <Masonmaag171@gmail.com>
2022-06-23 17:43:33 -0600

    deploy 9

--- server/index.js
+++ server/index.js
@@ -25,9 +25,10 @@ const {
     getAnswer
 } = listCtrl
 
- app.get('/js', (req,res) => {
-    res.sendFile(path.join(__dirname, './client/main.js'))
- })
+//  app.get('/main.js', (req,res) => {
+//     res.sendFile(path.join(__dirname, './client/main.js'))
+//  })
+app.use('/js', express.static(path.join(__dirname, 'client/main.js')))
 
 
 app.get("/api/compliment", getCompliment);
